c9d7c8d1e6a94bcadb0035eac8bfd05d6b32622e
SulemannShah <shahsuleman0077@gmail.com>
2025-05-14 04:01:08 +0500

    add

---
+++ main.py
@@ -0,0 +1,30 @@
+print("hello")
+
+# strings 
+
+a = "A"
+
+# converts characters to unicode
+print(ord(a))
+
+#converts unicode to characters
+print(chr(65))
+
+#indexing
+
+a = 4
+
+print(float(a))
+
+a = input("Enter your birth year")
+
+print(a)
+
+
+#comparion value
+
+a = 12
+b= 12
+
+print(a != b)
+print(45 > 5)
