6a52c51d6f51e39ff0dced940199b8cea7d5032e
SulemannShah <shahsuleman0077@gmail.com>
2025-05-14 20:20:19 +0500

    add

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ print(chr(65))
 
 a = 4
 
-print(float(a))
+# print(float(a))
 
-a = input("Enter your birth year")
+# a = input("Enter your birth year")
 
-print(a)
+# print(a)
 
 
 #comparion value
@@ -28,3 +28,87 @@ b= 12
 
 print(a != b)
 print(45 > 5)
+
+#if-else
+
+#a = 15
+
+#if a > 564:
+    #print("a is greater than 5")
+
+#else:
+    #print("a is less than 5")    
+
+#icecream = int(input("how many icecreams you have ?"))
+
+#if icecream == 1:
+#    print("give to me")
+
+#else:
+#    print("give to every one")
+
+
+#gender = str(input("enter your gender"))
+
+#if gender == "male":
+ #   print("good morning Sir")
+#elif gender == "female":
+ #   print("good moring Mam")  
+ # else:
+ # print("give a valid gender")  
+
+#number = int(input("enter your number"))
+
+#if number % 2 == 0:
+#    print("number is even")
+#else:
+#    print("number is odd")    #
+
+# for loop
+
+#d = range( 20,50 ,2)
+#for i in d:
+#    print(i)
+
+#n = int(input("enter a number..."))
+#for i in range(n):
+#    print("hello world")
+
+#n = int(input("enter a  number...")) 
+
+#for i in range(0 , n , 1):
+#    print(i)
+
+#n = 10
+#for i in range(10 , 0 , -1):
+#    print(i)
+
+#n = int(input("enter a number to prints it number ."))
+
+#for i in range( n , n*11 , n):
+#    print(i)
+
+#n = int(input("enter a number"))
+#sum = 0
+#for i in range(1 ,n+1):
+#    sum = sum + i
+#    print(sum)
+
+#n = int(input("enter a number"))
+
+#sum = 0
+
+#for i in range(1 , n+1):
+#    sum = sum + i
+    
+
+#print(sum)    
+
+n = int(input("enter a number"))
+
+fact = 1
+
+for i in range(1 , n+ 1):
+    fact = fact * i
+
+print(fact)    
